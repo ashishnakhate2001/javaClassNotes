758f2d4412966394a495849e9441a64d4360f685
Ashish Nakhate <ashishnakhate2001@gmail.com>
2024-02-13 21:48:05 +0530

    question 3

--- Unit 1/i Degisn/Main1.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-class Main1 {
-    public static void main(String[] args) {
-        //Fill your code here
-        System.out.print("Welcome to Amphi Event Management System");
-    }
-}
---
+++ Unit 1/i Design/Main3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main3{
+public static void main(String[] args) {
+
+    // Fill your code here
+    Scanner sc = new Scanner(System.in);
+ 
+
+ 
+        System.out.println("Enter the number of events hosted in Codissia");
+        int a = sc.nextInt();
+        System.out.println("Number of events hosted in Codissia is "+a);
+  
+}}
