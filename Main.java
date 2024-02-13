a0d2e08a492ea8336792eb3b46b33b11f6dbee5b
Ashish Nakhate <ashishnakhate2001@gmail.com>
2024-02-13 22:44:55 +0530

    Fixed .gitignore

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+class Main  { 
+    public static void main(String args[]) { 
+        int n; 
+     
+        Scanner sc = new Scanner(System.in); 
+        n = sc.nextInt();
+        int arr[] = new int[n];
+  
+        
+
+
+       
+    }
+} 
