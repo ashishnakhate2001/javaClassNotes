2db7a69e1e66303ee789aecaa50f7849c56ed7b5
Ashish Nakhate <ashishnakhate2001@gmail.com>
2024-02-13 22:26:57 +0530

    question 2 added

---
+++ Unit 1/i Design/2/Main2.java
@@ -0,0 +1,11 @@
+public class Main2{
+
+public static void main(String args[]){
+
+System.out.println("Arguments :");
+for(int i=0; i<args.length; i++){
+System.out.println(args[i]);
+}
+System.out.println("Number of arguments is "+args.length);
+}
+}
