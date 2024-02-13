e6e44e0e0308a637aa5013f897d6943f54acc08c
Ashish Nakhate <ashishnakhate2001@gmail.com>
2024-02-13 21:35:00 +0530

    i-design question2

---
+++ Unit 1/i Degisn/Main1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main1 {
+    public static void main(String[] args) {
+        //Fill your code here
+        System.out.print("Welcome to Amphi Event Management System");
+    }
+}
