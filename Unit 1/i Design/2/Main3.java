fe865c3aa866882c5d90caacfffca92f6b6211b0
Ashish Nakhate <ashishnakhate2001@gmail.com>
2024-02-13 22:30:53 +0530

    question 3 added

---
+++ Unit 1/i Design/2/Main3.java
@@ -0,0 +1,9 @@
+public class Main3{
+
+    public static void main(String args[]){
+
+        int a = Integer.parseInt(args[0]);
+
+        System.out.println("Square of the number :\n" + (a*a));
+    }
+}
