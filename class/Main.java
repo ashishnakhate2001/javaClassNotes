bf66c5a8cd328ca25e03f8a8eeab730f3e9923ff
Ashish Nakhate <ashishnakhate2001@gmail.com>
2024-02-15 20:27:26 +0530

    Question 1 added

--- class/Main.java
+++ class/Main.java
@@ -1,15 +1,32 @@
-import java.util.*; 
-class Main  { 
-    public static void main(String args[]) { 
-        int n; 
-        String flag = "Compatible";
-        Scanner sc = new Scanner(System.in); 
-        n = sc.nextInt();
-        int arr[] = new int[n];
-  
-        
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-       
+        int n = sc.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int distinctCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int j;
+            for (j = 0; j < i; j++) {
+                if (array[i] == array[j]) {
+                    break;
+                }
+            }
+
+            // If no duplicate element found, increment distinctCount
+            if (i == j) {
+                distinctCount++;
+            }
+        }
+
+        System.out.println(distinctCount);
     }
-} 
+}
